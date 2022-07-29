5a3ee8cf6ed2409a95aa7cb10a639e4257ed08b5
Yago Lourenço <yagoasl@gmail.com>
2022-07-29 17:29:18 -0300

    Admin atualizado

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import LivroReceitas
 
-admin.site.register(LivroReceitas)
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita','categoria','tempo_de_preparo')
+    list_display_links = ('id', 'nome_receita')
 
+admin.site.register(LivroReceitas, ListandoReceitas)
 
+#loguin localhost:8000/admin
+#user = admin
+#pass = 123456
