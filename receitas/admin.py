cc37f5eac24218a75ac542f9172d04e292794e6e
Yago Lourenço <yagoasl@gmail.com>
2022-07-29 17:53:36 -0300

    Atualização admin.py

--- receitas/admin.py
+++ receitas/admin.py
@@ -4,6 +4,9 @@ from .models import LivroReceitas
 class ListandoReceitas(admin.ModelAdmin):
     list_display = ('id', 'nome_receita','categoria','tempo_de_preparo')
     list_display_links = ('id', 'nome_receita')
+    search_fields = ('nome_receita',)
+    list_filter = ('categoria',)
+    list_per_page = 10
 
 admin.site.register(LivroReceitas, ListandoReceitas)
 
